3d7ff79d4c101ff9c067a878ef6f6d65fc8c2bab
DaniOracle <152621795+DaniOracle@users.noreply.github.com>
2025-12-18 09:38:06 +0000

    Add dark theme to app
    - Create color palette
    
    - Apply theme across the entire app
    
    - Add CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/* React */
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+/* MUI */
+import { CssBaseline } from '@mui/material';
+
+/* Theme */
+import { createTheme, type ThemeOptions, ThemeProvider } from '@mui/material';
+
+/* Components. The root component which contains router logic */
+import App from './App'
+
+/* Colour palette definition */
+export const darkTheme: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary:{
+      main: '#5865F2',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#7289da',  
+    },
+    background: {
+      default: '#36393f',
+      paper: '#2f3136',
+    },
+    text: {
+      primary: '#ffffff',
+      secondary: '#b9bbbe'
+    },
+    divider: '#4f545c',
+  }
+};
+const theme = createTheme(darkTheme);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>  
+          <App />
+      <CssBaseline />
+    </ThemeProvider>
   </StrictMode>,
 )
